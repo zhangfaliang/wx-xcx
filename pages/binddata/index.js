7bc10e01d63868e247af464c14df0144e5aca6a8
张发亮 <fl.zhang@8win.com>
2018-07-11 13:57:32 +0800

    add bind data

---
+++ pages/binddata/index.js
@@ -0,0 +1,25 @@
+Page({
+  data: {
+    text: '数据绑定',
+    id: 0,//组件内使用需要双引号内使用
+    condition: true,
+    length: 6,
+    name: 'shiny',
+    object: {
+      key:'test'
+    },
+    array: ['MINA'],
+    zero: 0,
+    a: 1,
+    b: 2,
+    obj1: {
+      a: 10,
+      b: 9,
+    },
+    obj2: {
+      c: 8
+    },
+    e:7
+    
+  }
+})
