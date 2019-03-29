eb4307c10682c52a19048217c218d6281e7b00b5
张发亮 <fl.zhang@8win.com>
2019-03-29 16:51:34 +0800

    更新小程序

--- app.js
+++ app.js
@@ -38,7 +38,9 @@ App({
   },
   onShow: function (options) {
     // Do something when show.
-
+    wx.showShareMenu({
+      withShareTicket: true
+     })
   },
   onHide: function () {
     // Do something when hide.
--- pages/EatChicken/index.js
+++ pages/EatChicken/index.js
@@ -14,6 +14,8 @@ Page({
       { title: '新手吃鸡必看 绝地求生大逃杀生存指南',imgUrl:'http://5b0988e595225.cdn.sohucs.com/images/20171113/fbf8aae351cd4049bf8f29ec22b70e96.jpeg',idNum:"01" },
       { title:'新手吃鸡，自定义操作',imgUrl:'http://img5.imgtn.bdimg.com/it/u=3159449345,3781131544&fm=27&gp=0.jpg',idNum:"02" },
       { title: '绝地求生刺激战场怎么瞄准', imgUrl: 'http://pic.uzzf.com/up/2017-9/20179259513989.png',idNum:"03" },
+      { title: '超级干货贴! 教你如何用手机操作稳吃鸡', imgUrl: 'http://img.18183.com/uploads/allimg/171107/168-1G10GI353235.jpg', idNum: "04" },
+      { title: '超级干货贴! 教你如何用手机操作稳吃鸡', imgUrl: 'http://img.18183.com/uploads/allimg/171107/168-1G10GI353235.jpg', idNum: "04" },
       { title:'超级干货贴! 教你如何用手机操作稳吃鸡',imgUrl:'http://img.18183.com/uploads/allimg/171107/168-1G10GI353235.jpg',idNum:"04" },
     ] ,
     
