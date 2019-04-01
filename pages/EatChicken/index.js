9ef63e8530985ca1094665ee8c3ed5555a37b0a5
张发亮 <fl.zhang@8win.com>
2019-04-01 10:52:28 +0800

    添加广告位

--- pages/EatChicken/index.js
+++ pages/EatChicken/index.js
@@ -15,8 +15,6 @@ Page({
       { title:'新手吃鸡，自定义操作',imgUrl:'http://img5.imgtn.bdimg.com/it/u=3159449345,3781131544&fm=27&gp=0.jpg',idNum:"02" },
       { title: '绝地求生刺激战场怎么瞄准', imgUrl: 'http://pic.uzzf.com/up/2017-9/20179259513989.png',idNum:"03" },
       { title: '超级干货贴! 教你如何用手机操作稳吃鸡', imgUrl: 'http://img.18183.com/uploads/allimg/171107/168-1G10GI353235.jpg', idNum: "04" },
-      { title: '超级干货贴! 教你如何用手机操作稳吃鸡', imgUrl: 'http://img.18183.com/uploads/allimg/171107/168-1G10GI353235.jpg', idNum: "04" },
-      { title:'超级干货贴! 教你如何用手机操作稳吃鸡',imgUrl:'http://img.18183.com/uploads/allimg/171107/168-1G10GI353235.jpg',idNum:"04" },
     ] ,
     
     footer: ''
