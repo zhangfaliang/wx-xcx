e9f16aa576dec3544f3d85ba23931832e99b8ae0
YanHong Fan <1254834557@qq.com>
2019-04-02 20:35:29 +0800

    去掉无用代码

--- common/index.js
+++ common/index.js
@@ -1,16 +1,11 @@
 var base = require('../lib/base/dist/base'),
-    util = require('./util/index'),
-    config = require('./config/index'),
     helper = require('./helper/index'),
     spider = require('../lib/mina-spider/src/index');
 
 var common = {
-    util: base.core.extend(util, base.util),
     helper: base.core.extend(helper, base.helper),
     core: base.core,
     date: base.date,
-    config: config,
-    // login: login,
     spider: spider,
 };
 
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,5 +1,4 @@
 //logs.js
-const util = require('../../utils/util.js')
 
 Page({
   data: {
