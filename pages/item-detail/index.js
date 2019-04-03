7a1c2069fb205949919b0f636e331ce6a756bbc1
张发亮 <fl.zhang@8win.com>
2019-04-03 10:00:23 +0800

    添加 新的吃鸡版本

--- pages/item-detail/index.js
+++ pages/item-detail/index.js
@@ -1,177 +1,220 @@
 Page({
   behaviors: [],
   data: {
+    unitIds: [
+      "adunit-feda5011874873b6",
+      "adunit-158f9ba9d8a17efd",
+      "adunit-2ecbbb8d9a9baa6c",
+      "adunit-849fa37b6f759659",
+      "adunit-81165735c25c8e44",
+      "adunit-16e1088ce22e77de",
+      "adunit-d76dc13d06dd605b",
+      "adunit-8363446d205805bd",
+      "adunit-c862f8d245fda077",
+      "adunit-af8be95a81757654"
+    ],
     "01": [
       {
         content:
           "1、开局观察飞机上的人数游戏开始时注意切换飞机的视角，观察还有多少玩家没有跳伞以及现在是否有玩家正在跳伞。",
-        imgUrl: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3379831367,107520778&fm=11&gp=0.jpg"
+        imgUrl:
+          "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3379831367,107520778&fm=11&gp=0.jpg"
       },
       {
         content:
           "2、决定跳伞方式,跳伞过程中如果想最短时间落地就往地面俯冲，这样可以获得最快的下落速度，先落地的基本能占得先机，先拿枪秒杀后落地的玩家。如果想飞的更远就平行飞，这样可以飞更远的距离但是落地时间相对较长。",
 
-        imgUrl: "http://img3.imgtn.bdimg.com/it/u=1256622995,3893971298&fm=27&gp=0.jpg"
+        imgUrl:
+          "http://img3.imgtn.bdimg.com/it/u=1256622995,3893971298&fm=27&gp=0.jpg"
       },
       {
         content:
-        "3、打开降落伞观察周围情况,拖动小眼睛可以观察人物周围的环境并且不影响人物现在的动作，打开降落伞后需要观察你的附近是否有人将要和你去同一个地方，观察敌人的落地点在哪里以免被莫名击杀，如果你比他先落地可以装备好枪后去暗杀他。",
+          "3、打开降落伞观察周围情况,拖动小眼睛可以观察人物周围的环境并且不影响人物现在的动作，打开降落伞后需要观察你的附近是否有人将要和你去同一个地方，观察敌人的落地点在哪里以免被莫名击杀，如果你比他先落地可以装备好枪后去暗杀他。",
 
-        imgUrl: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3947252738,1719063838&fm=11&gp=0.jpg"
+        imgUrl:
+          "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3947252738,1719063838&fm=11&gp=0.jpg"
       },
       {
         content:
-        "4、快速搜查装备,落地后迅速跑到附近的物资点搜集物品，在物品附近可以观察到附近地面上的物品，将物品进行拖动即可准确的装备你所需要的道具，可以防止出现漏拿物品的现象，对于不要的物品可以直接拖动移出。同样对被击杀敌人附近的包裹以及飞机空投箱依旧有用。",
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532426905931&di=a91d8f1aa74d545a03e7d9d7e2f0c51a&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180704%2F0f5562ff8dd84834962bc3bbc92b23ce.jpeg"
+          "4、快速搜查装备,落地后迅速跑到附近的物资点搜集物品，在物品附近可以观察到附近地面上的物品，将物品进行拖动即可准确的装备你所需要的道具，可以防止出现漏拿物品的现象，对于不要的物品可以直接拖动移出。同样对被击杀敌人附近的包裹以及飞机空投箱依旧有用。",
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532426905931&di=a91d8f1aa74d545a03e7d9d7e2f0c51a&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180704%2F0f5562ff8dd84834962bc3bbc92b23ce.jpeg"
       },
       {
         content:
-        "5、丢出指定数量的物品,当你的队友需要你给他道具的时候，你可以打开背包，按住并且将物品拖出背包，即可丢出指定数量的药品和子弹。如果你不会这个操作，你的几百发子弹也只能丢一次了，丢完后自己就没有子弹了。",
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427248935&di=520594781c2ce81264b86598e937b859&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180620%2F419de9815d174028aee070f8de7f669f.jpeg"
+          "5、丢出指定数量的物品,当你的队友需要你给他道具的时候，你可以打开背包，按住并且将物品拖出背包，即可丢出指定数量的药品和子弹。如果你不会这个操作，你的几百发子弹也只能丢一次了，丢完后自己就没有子弹了。",
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427248935&di=520594781c2ce81264b86598e937b859&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180620%2F419de9815d174028aee070f8de7f669f.jpeg"
       },
       {
         content:
-        "6、载具上快速换位,在载具上我们可以换位键快速在载具上实现换位，省去了上下车换座位的时间以及可能带来的各种危险。在左下角我们可以看到载具的座位数量，灰色的方块代表该座位无人可以换座，白色的方块则表示该座位有人了无法换位。",
-        imgUrl: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1843195006,3130106411&fm=11&gp=0.jpg"
+          "6、载具上快速换位,在载具上我们可以换位键快速在载具上实现换位，省去了上下车换座位的时间以及可能带来的各种危险。在左下角我们可以看到载具的座位数量，灰色的方块代表该座位无人可以换座，白色的方块则表示该座位有人了无法换位。",
+        imgUrl:
+          "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1843195006,3130106411&fm=11&gp=0.jpg"
       },
       {
         content:
-        "7、脱鞋进行游戏,穿鞋子在屋内跑动时会发出较大的声音，一般高手玩游戏都喜欢脱掉鞋子，这样可以减少被敌人发现的概率。",
-        imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427087436&di=990ea001479c2cd5eb0fd627cacd3dae&imgtype=0&src=http%3A%2F%2Fwww.crsky.com%2Fuploadfiles%2Farticle%2F2018%2F06%2F17%2F2018061720481984.png'
+          "7、脱鞋进行游戏,穿鞋子在屋内跑动时会发出较大的声音，一般高手玩游戏都喜欢脱掉鞋子，这样可以减少被敌人发现的概率。",
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427087436&di=990ea001479c2cd5eb0fd627cacd3dae&imgtype=0&src=http%3A%2F%2Fwww.crsky.com%2Fuploadfiles%2Farticle%2F2018%2F06%2F17%2F2018061720481984.png"
       },
       {
         content:
-        "8、伪装成幻影坦克,利用地形上的草丛进行掩护，在草丛处蹲下，只要敌人距离你很远或者没有仔细观察，基本上是看不到你的，你需要做的就是拖动小眼睛观察四周的情况，如果发现敌人后用你的枪对准他的脑袋就行了。",
-        imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533021734&di=29e79cdc0615e368f538e318af9a777c&imgtype=jpg&er=1&src=http%3A%2F%2Fimgsa.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D4e034f600655b3199cf9827573a88286%2F1c950a7b02087bf4eb115488fed3572c10dfcfd6.jpg'
+          "8、伪装成幻影坦克,利用地形上的草丛进行掩护，在草丛处蹲下，只要敌人距离你很远或者没有仔细观察，基本上是看不到你的，你需要做的就是拖动小眼睛观察四周的情况，如果发现敌人后用你的枪对准他的脑袋就行了。",
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533021734&di=29e79cdc0615e368f538e318af9a777c&imgtype=jpg&er=1&src=http%3A%2F%2Fimgsa.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D4e034f600655b3199cf9827573a88286%2F1c950a7b02087bf4eb115488fed3572c10dfcfd6.jpg"
       },
       {
         content:
-        "9、S形蛇皮走位, 被敌人发现后如果无法辨认敌人的位置或者现在不适合交火，逃跑的时候按住自动奔跑键并左右晃动进行蛇皮走位，如果你只是直线跑动的话，你的脑袋就危险了。S形蛇皮走位",
+          "9、S形蛇皮走位, 被敌人发现后如果无法辨认敌人的位置或者现在不适合交火，逃跑的时候按住自动奔跑键并左右晃动进行蛇皮走位，如果你只是直线跑动的话，你的脑袋就危险了。S形蛇皮走位",
 
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533022021&di=f7f0d38b3fdc8f86c2ef4fdb020ea484&imgtype=jpg&er=1&src=http%3A%2F%2Fwx1.sinaimg.cn%2Fmw690%2Fa3610f35ly1fodiw90vb5g20b4069qv6.gif"
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533022021&di=f7f0d38b3fdc8f86c2ef4fdb020ea484&imgtype=jpg&er=1&src=http%3A%2F%2Fwx1.sinaimg.cn%2Fmw690%2Fa3610f35ly1fodiw90vb5g20b4069qv6.gif"
       },
       {
         content:
-        "10、跑毒阴人请贴边走,贴边走的好处可以降低敌人从背后出现的概率，而且在对面跑毒的过程中你在安全区附近还能顺便收几个人头。如果时间够的话还能跑到你击杀敌人的附近拿走他们的物资。",
+          "10、跑毒阴人请贴边走,贴边走的好处可以降低敌人从背后出现的概率，而且在对面跑毒的过程中你在安全区附近还能顺便收几个人头。如果时间够的话还能跑到你击杀敌人的附近拿走他们的物资。",
 
-        imgUrl: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1958897384,3092687212&fm=27&gp=0.jpg"
+        imgUrl:
+          "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1958897384,3092687212&fm=27&gp=0.jpg"
       },
       {
         content:
-        "11、厕所战神,很多人喜欢跑到安全区内的厕所里面阴人，他们会进去然后关上门，等有人开门的时候直接将他击毙并拿走他的物资。厕所战神需要找一个安全区内的厕所，然后进去关门，把你的枪头对准门就行了，一旦有人开门直接杀了他。",
+          "11、厕所战神,很多人喜欢跑到安全区内的厕所里面阴人，他们会进去然后关上门，等有人开门的时候直接将他击毙并拿走他的物资。厕所战神需要找一个安全区内的厕所，然后进去关门，把你的枪头对准门就行了，一旦有人开门直接杀了他。",
 
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427405675&di=ef07ff6fc789bc2ed49ee758b9df358c&imgtype=0&src=http%3A%2F%2Fi3.img.969g.com%2Fpub%2Fimgx2018%2F07%2F10%2F503_171250_c6041.jpg"
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427405675&di=ef07ff6fc789bc2ed49ee758b9df358c&imgtype=0&src=http%3A%2F%2Fi3.img.969g.com%2Fpub%2Fimgx2018%2F07%2F10%2F503_171250_c6041.jpg"
       },
       {
         content:
-        "12、反厕所战神,对于喜欢蹲厕所的人，我们有着一套专门对付这些人的办法。在厕所门口，对准厕所的窗户，把你事先准备好的燃烧弹、手雷丢进去。你只需等待提示击杀完成后，就可以去搜刮他的财产了，如果对方跑了出来你就对准大门直接将他击杀，他刚才怎么蹲你的，你就怎么对付他。",
+          "12、反厕所战神,对于喜欢蹲厕所的人，我们有着一套专门对付这些人的办法。在厕所门口，对准厕所的窗户，把你事先准备好的燃烧弹、手雷丢进去。你只需等待提示击杀完成后，就可以去搜刮他的财产了，如果对方跑了出来你就对准大门直接将他击杀，他刚才怎么蹲你的，你就怎么对付他。",
 
-        imgUrl: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2763486393,1392186228&fm=27&gp=0.jpg"
+        imgUrl:
+          "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2763486393,1392186228&fm=27&gp=0.jpg"
       },
       {
         content:
-        "13、空投箱附近的菜鸟驿站, 如果你发现了空投的位置，并且你身上正好有高倍镜，你只需要瞄准空投，等待快递员在这里取件的时候对准他的脑袋来一发，随着死亡的人数增多，还是会有不怕死的人来这里捡别人的快递还有空投的。因为每个人都有侥幸的心理，他们也希望拿到最好的装备。",
+          "13、空投箱附近的菜鸟驿站, 如果你发现了空投的位置，并且你身上正好有高倍镜，你只需要瞄准空投，等待快递员在这里取件的时候对准他的脑袋来一发，随着死亡的人数增多，还是会有不怕死的人来这里捡别人的快递还有空投的。因为每个人都有侥幸的心理，他们也希望拿到最好的装备。",
 
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427519854&di=87f4c702dbe841c67d0c53f5c27ed3df&imgtype=0&src=http%3A%2F%2Fimg.soyohui.com%2Fupdate%2Fueditor%2F2018%2F0606%2F5b17ac1a9ff50.jpg"
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427519854&di=87f4c702dbe841c67d0c53f5c27ed3df&imgtype=0&src=http%3A%2F%2Fimg.soyohui.com%2Fupdate%2Fueditor%2F2018%2F0606%2F5b17ac1a9ff50.jpg"
       },
       {
         content:
-        "14、雾天秋名山车神,在雾天，由于视野的限制，就算在很小的决赛圈里，也有很多人是趴在那里不动的。因为周围都是人，作为一个秋名山车神，正确的做法就是开着汽车(最好是吉普车)，边按喇叭边绕圈，有机会碾死那些趴下的人，如果有人朝你射击你就直接冲过去碾碎他，按喇叭是告诉他，他的死期到了。曾经有人用这招在雾天的决赛圈内获胜，有一部分人都是被碾压致死的。TIP：天气晴朗和雨天不要用这招，否则你就是找死，喇叭的作用是吓唬对手，让对手挪动位置，并且告诉他老子已经发现你了，你有本事别跑。",
+          "14、雾天秋名山车神,在雾天，由于视野的限制，就算在很小的决赛圈里，也有很多人是趴在那里不动的。因为周围都是人，作为一个秋名山车神，正确的做法就是开着汽车(最好是吉普车)，边按喇叭边绕圈，有机会碾死那些趴下的人，如果有人朝你射击你就直接冲过去碾碎他，按喇叭是告诉他，他的死期到了。曾经有人用这招在雾天的决赛圈内获胜，有一部分人都是被碾压致死的。TIP：天气晴朗和雨天不要用这招，否则你就是找死，喇叭的作用是吓唬对手，让对手挪动位置，并且告诉他老子已经发现你了，你有本事别跑。",
 
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427623752&di=9f48362b8689ad3bbdbd4e6048dd3ef4&imgtype=0&src=http%3A%2F%2Fimgsa.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D707fab2bc53d70cf4cfaaa0dc8dcd1ba%2F7a899e510fb30f244de61511c495d143ad4b03a7.jpg"
-      },
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532427623752&di=9f48362b8689ad3bbdbd4e6048dd3ef4&imgtype=0&src=http%3A%2F%2Fimgsa.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D707fab2bc53d70cf4cfaaa0dc8dcd1ba%2F7a899e510fb30f244de61511c495d143ad4b03a7.jpg"
+      }
     ],
     "02": [
       {
-        content:
-          "第一步，首先创建角色，进入游戏，点击设置按钮键",
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533022659&di=32206692e707e63f2d8fd315f46f86fe&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.zd51.cn%2Fzdyy-zd51cn%2Fallimg%2F180224%2F09521T2I-3.jpg"
+        content: "第一步，首先创建角色，进入游戏，点击设置按钮键",
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533022659&di=32206692e707e63f2d8fd315f46f86fe&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.zd51.cn%2Fzdyy-zd51cn%2Fallimg%2F180224%2F09521T2I-3.jpg"
       },
       {
         content:
           "第二步，进入基础设置界面，新手建议默认设置，老手的话建议开启陀螺仪和左右探头射击。",
-        imgUrl: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1622601076,1176205569&fm=15&gp=0.jpg"
+        imgUrl:
+          "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1622601076,1176205569&fm=15&gp=0.jpg"
       },
       {
-        content:
-          " 第三步，你可以通过自己游戏体验来设置画面设置。",
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428005625&di=d09b72f06ffad920aaf9b22f2973c9f4&imgtype=0&src=http%3A%2F%2Fimg1.gtimg.com%2Fcq%2Fpics%2Fhv1%2F21%2F148%2F2273%2F147839586.png"
+        content: " 第三步，你可以通过自己游戏体验来设置画面设置。",
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428005625&di=d09b72f06ffad920aaf9b22f2973c9f4&imgtype=0&src=http%3A%2F%2Fimg1.gtimg.com%2Fcq%2Fpics%2Fhv1%2F21%2F148%2F2273%2F147839586.png"
       },
       {
         content:
           " 第四步，操作设置你可以调节按钮的大小和透明度，以及按钮的位置，你看着怎么舒服怎么来.",
-        imgUrl:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428046932&di=02a2e2b55825a36d511f326472934e8f&imgtype=0&src=http%3A%2F%2Fpic4.zhimg.com%2Fv2-8dd264c9e7b0555c0a15fcef2f0c2c9f_b.jpg"
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428046932&di=02a2e2b55825a36d511f326472934e8f&imgtype=0&src=http%3A%2F%2Fpic4.zhimg.com%2Fv2-8dd264c9e7b0555c0a15fcef2f0c2c9f_b.jpg"
       },
       {
         content:
           " 第五步，驾驶汽车设置，有方向设置和摇杆设置，个人建议选择第三种摇杆设置，体验效果更好。",
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428205245&di=0f241b0131b906de1cf2c182c3305767&imgtype=0&src=http%3A%2F%2Fimg5.pcpop.com%2FArticleImages%2Ffnw%2F2018%2F0209%2Fae3312c4-5850-48a0-b3cf-846a1afc6a7a.jpg"
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428205245&di=0f241b0131b906de1cf2c182c3305767&imgtype=0&src=http%3A%2F%2Fimg5.pcpop.com%2FArticleImages%2Ffnw%2F2018%2F0209%2Fae3312c4-5850-48a0-b3cf-846a1afc6a7a.jpg"
       },
       {
         content:
           " 第六步，灵敏度调节，前期新手灵敏度建议低一点，到后期上手了可以适当调高一点灵敏度。这样出枪速度更快。",
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428242682&di=ab634bf147b5e5a88dc6f2b1296770d0&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1807%2F18%2F11489126_2018071779381173_thumb.jpg"
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428242682&di=ab634bf147b5e5a88dc6f2b1296770d0&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1807%2F18%2F11489126_2018071779381173_thumb.jpg"
       },
       {
         content:
           " 第七步，拾取设置，你可以开启自动拾取，建议关闭，这样有利于更好的拾取装备，只拿好装备。",
-        imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428276666&di=46aac2e4dd90699e8e46b9c7665a085d&imgtype=0&src=http%3A%2F%2Fwww.gmz88.com%2Fuploadimg%2Fimage%2F20180210%2F20180210164921_74075.jpg"
-      },
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428276666&di=46aac2e4dd90699e8e46b9c7665a085d&imgtype=0&src=http%3A%2F%2Fwww.gmz88.com%2Fuploadimg%2Fimage%2F20180210%2F20180210164921_74075.jpg"
+      }
     ],
     "03": [
       {
         content:
           "首先当我们发现敌人时，不能轻举妄动，要在不被发现的情况下一举拿下对方人头。",
-          imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428418742&di=a9bd10e2e296058ee58dbfe8498d758c&imgtype=0&src=http%3A%2F%2Fimg.18183.com%2Fuploads%2Fallimg%2F180713%2F216-1PG3100330-50.jpg%40%252118183"
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428418742&di=a9bd10e2e296058ee58dbfe8498d758c&imgtype=0&src=http%3A%2F%2Fimg.18183.com%2Fuploads%2Fallimg%2F180713%2F216-1PG3100330-50.jpg%40%252118183"
       },
       {
         content:
           "如果武器是持有高倍镜的前提下沫沫不建议开镜瞄准，毕竟像四倍镜八倍镜那些，一开镜视野就变小了，半天找不到人，沫沫可有过手持八倍镜瞄准的时候被近处的一个敌人直接跑到跟前被秒掉了呢。（如果是红点全息等开镜后瞄准和瞄准后开镜问题都不大。）",
-          imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428521178&di=e66284732971835791ded749a718b0c8&imgtype=0&src=http%3A%2F%2Fimage.9game.cn%2F2018%2F3%2F7%2F19696969.jpg"      },
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428521178&di=e66284732971835791ded749a718b0c8&imgtype=0&src=http%3A%2F%2Fimage.9game.cn%2F2018%2F3%2F7%2F19696969.jpg"
+      },
       {
         content:
           "这时候的正确做法应该是先对敌人进行预瞄，意思就是先将准星对准敌人。",
-          imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428614073&di=376fc5b56c152ec54965075ab9020103&imgtype=0&src=http%3A%2F%2Fimg4.dwstatic.com%2Fpg%2F1803%2F384784219912%2F384784370717.jpeg"      },
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428614073&di=376fc5b56c152ec54965075ab9020103&imgtype=0&src=http%3A%2F%2Fimg4.dwstatic.com%2Fpg%2F1803%2F384784219912%2F384784370717.jpeg"
+      },
       {
         content:
           "接下来再点击右侧的瞄准镜开镜，这时敌人就会处于自己的视野中，而不是开镜后慢慢寻找，这样不仅加大了难度也给自己带来了威胁",
-          imgUrl: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=63816821,3568428027&fm=11&gp=0.jpg"      },
+        imgUrl:
+          "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=63816821,3568428027&fm=11&gp=0.jpg"
+      },
       {
         content:
           "另外如果像增加瞄准敌人的几率，可以在设置中将辅助瞄准和陀螺仪打开，二者都有追踪瞄准的作用，不过辅助瞄准开启后好像爆头更加难了，而陀螺仪灵敏度需要很好地进行调整，灵敏度太高只有手机屏幕轻微晃动，镜头的晃动幅度也会相应变大，且方向相反，建议小伙伴们选择开镜开启，否则玩起来很容易头晕。",
-          imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428659091&di=32365149d49e797439aa0fe4f06f41e1&imgtype=0&src=http%3A%2F%2Fi1.img.969g.com%2Fpub%2Fimgx2018%2F06%2F10%2F503_210933_0a264.jpg"      },
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428659091&di=32365149d49e797439aa0fe4f06f41e1&imgtype=0&src=http%3A%2F%2Fi1.img.969g.com%2Fpub%2Fimgx2018%2F06%2F10%2F503_210933_0a264.jpg"
+      },
       {
         content:
           "除此之外还要多家练习，训练场就是个供小伙伴们联系技术的地方，没事多去练练，对自己会有很大提升。",
-          imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428769877&di=983a18efb37272f7b3ff0c386c296d12&imgtype=0&src=http%3A%2F%2Fi2.img.969g.com%2Fpub%2Fimgx2018%2F06%2F15%2F503_010900_4d268.jpg"      },
+        imgUrl:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532428769877&di=983a18efb37272f7b3ff0c386c296d12&imgtype=0&src=http%3A%2F%2Fi2.img.969g.com%2Fpub%2Fimgx2018%2F06%2F15%2F503_010900_4d268.jpg"
+      }
     ],
     "04": [
       {
-        content:
-          "绝地求生刺激战场新手基本操作攻略",
-        imgUrl: "//img0.pconline.com.cn/pconline/1802/24/10864812_301576_600x600.jpg"
+        content: "绝地求生刺激战场新手基本操作攻略",
+        imgUrl:
+          "//img0.pconline.com.cn/pconline/1802/24/10864812_301576_600x600.jpg"
       },
       {
         content:
           "　　在进入游戏之后，游戏的左上方会显示剩余玩家数量与玩家击败敌人数量。通过这项数据，玩家可以直观地判断自身需要以怎样的战术稳定吃鸡。",
-        imgUrl: "//img0.pconline.com.cn/pconline/1802/24/10864812_301577_600x600.jpg"
+        imgUrl:
+          "//img0.pconline.com.cn/pconline/1802/24/10864812_301577_600x600.jpg"
       },
       {
         content:
           "　　在屏幕的正下方，玩家可以进行补充药品、切换枪械、使用近战武器、装填子弹等操作。如果玩家的血量并非满的，那么可根据实际情况选择使用不同的药品。若是不知道药品的效果，打开背包后点击药品便可以查看相关信息。切换枪械是为了适应于各种地形作战，且玩家在与敌人相互猛攻时，将其中一把枪械的一梭子弹打光，这种情况下填充子弹是很危险的一种行为。在这迫在眉睫之际，玩家可以切换至另一把枪械，用其攻击敌人，简单快速。近战武器在游戏前期是很实用的，其造成的伤害远远高于拳头。萌新玩家在打出数发子弹后，可能会忘记装填子弹这一操作，若之后与敌人相遇，很有可能便会吃亏。　　在屏幕的右上方，标注着地图、持续奔跑以及自由视角等。萌新玩家要养成一个良好的习惯——随时随地查看地图，这样才能明确自身的目的，确认自己接下来应该前往哪里。且通过地图，玩家可以获知很多信息，例如演习信号、枪声方位等。持有奔跑是一项简易操作，实用性强，可以在玩家直线前往某一处区域时使用。而自由视角的作用较大，却总是被萌新玩家忽视。按住这个按钮后，玩家可以观察周围敌情变化，从而做出相应调整，在持续奔跑与蹲守某处时都可以使用这项操作。",
-        imgUrl: "//img0.pconline.com.cn/pconline/1802/24/10864812_301579_600x600.jpg"
+        imgUrl:
+          "//img0.pconline.com.cn/pconline/1802/24/10864812_301579_600x600.jpg"
       },
       {
         content:
           "　　每一项基础操作都有其存在的意义，萌新玩家不要过于忽略某一项基础操作的重要性。在操作设置界面中，玩家可根据自身喜好调节各类操作。例如左右探头、智能拾取物资等，在游戏中操作别扭时，点击屏幕右上角的设置按钮即可进行调整修改。",
         imgUrl: ""
-      },
+      }
     ]
   }, // 私有数据，可用于模版渲染 <p></p>
-  onLoad: function(options) {  
-    this.setData({  
-      pageData: this.data[options.idNum]  
-    })  
-  }  
-
+  onLoad: function(options) {
+    this.setData({
+      pageData: this.data[options.idNum]
+    });
+  }
 });
